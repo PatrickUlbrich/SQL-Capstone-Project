a8ebf4d2097962ac1396c90c3ad5cecf5c017b24
Patrick Ulbrich <94012276+PatrickUlbrich@users.noreply.github.com>
2021-11-09 21:37:42 +0100

    Update bike-sharing SQL scripts.sql

--- bike-sharing SQL scripts.sql
+++ bike-sharing SQL scripts.sql
@@ -1,5 +1,5 @@
 /*
-Zunächst sehen wir uns die ersten 1000 Einträge an, um uns die Daten anzusehen. */
+Zunächst sehen wir uns die ersten 1000 Einträge zur Sichtung der Daten an. */
 SELECT *
 FROM 202109_divvy_tripdata
 LIMIT 1000;
@@ -7,7 +7,7 @@ LIMIT 1000;
 
 /*
 Wir überprüfen als Erstes, ob sich in einer Tabelle Duplikate befinden.
-Es werden die ride_ids einzeln gezählt. Sollte eine ride_id doppelt vorhanden sein (Zähler größer 1), wird diese ausgegeben. */
+Es werden die ride_ids einzeln gezählt. Sollte eine ride_id doppelt vorhanden sein (Zähler größer 1), wird diese ride_id ausgegeben. */
 SELECT
 	ride_id,
 	COUNT(ride_id)
@@ -79,16 +79,16 @@ Zum Zwecke dieser Analyse nehmen wir an, dass sich nach Rücksprache herausgeste
 Wir sollen für die betroffenen Zeilen die Daten entsprechend bearbeiten, dass die Zeiten wieder in der richtigen Reihenfolge eingetragen sind.
 Dazu nutzen wir zunächst eine temporäre Tabelle, um sicherzustellen, dass die Abfrage funktioniert. */
 
-CREATE TEMPORARY TABLE column_inverted_check 
+CREATE TEMPORARY TABLE time_change_check 
 SELECT * 
 FROM 202109_divvy_tripdata;
 
 -- wir fügen eine Spalte als Platzhalter hinzu
-ALTER TABLE column_inverted_check
+ALTER TABLE time_change_check
 ADD COLUMN starttime DATETIME;
 
 -- für Zeilen, die eine negative Fahrzeit haben, machen wir die Startzeit zur Endzeit und umgekehrt. 
-UPDATE column_inverted_check
+UPDATE time_change_check
 SET 
 	starttime = started_at,
 	started_at = ended_at,
@@ -96,30 +96,30 @@ SET
 WHERE TIMESTAMPDIFF(SECOND, started_at, ended_at) < 0;
 
 -- Anschließend entfernen wir die Platzhalter-Spalte wieder. 
-ALTER TABLE column_inverted_check
+ALTER TABLE time_change_check
 DROP COLUMN starttime;
 
 
 /*
 Es existiert nun eine temporäre Tabelle.
-Wir vergleichen die Werte der neuen Tabelle mit denen der alten Tabelle für die Stellen, bei denen die Fahrzeit in der Original-Tabelle negativ ist.
+Wir vergleichen die Werte der temporären Tabelle mit denen der alten Tabelle für die Stellen, bei denen die Fahrzeit in der Original-Tabelle negativ ist.
 Es werden die Startzeiten und Endzeiten und die berechnete Fahrzeit beider Tabellen abgefragt und die ride_id wird als Vergleichspunkt genutzt. */
 SELECT 
 	202109_divvy_tripdata.ride_id,
 	202109_divvy_tripdata.started_at AS original_start,
 	202109_divvy_tripdata.ended_at AS original_end,
-	column_inverted_check.started_at AS changed_start,
-	column_inverted_check.ended_at AS changed_end,
+	time_change_check.started_at AS changed_start,
+	time_change_check.ended_at AS changed_end,
 	TIMESTAMPDIFF(SECOND, 202109_divvy_tripdata.started_at, 202109_divvy_tripdata.ended_at) AS original_ride_duration,
-	TIMESTAMPDIFF(SECOND, column_inverted_check.started_at, column_inverted_check.ended_at) AS changed_ride_duration
+	TIMESTAMPDIFF(SECOND, time_change_check.started_at, time_change_check.ended_at) AS changed_ride_duration
 FROM 
 	202109_divvy_tripdata
 INNER JOIN 
-	column_inverted_check
-ON 202109_divvy_tripdata.ride_id = column_inverted_check.ride_id
+	time_change_check
+ON 202109_divvy_tripdata.ride_id = time_change_check.ride_id
 WHERE TIMESTAMPDIFF(SECOND, 202109_divvy_tripdata.started_at, 202109_divvy_tripdata.ended_at) < 0
-ORDER BY TIMESTAMPDIFF(SECOND, column_inverted_check.started_at, column_inverted_check.ended_at) DESC;
--- Wir sehen, dass die Änderungen korrekt durchgeführt wurden.
+ORDER BY TIMESTAMPDIFF(SECOND, time_change_check.started_at, time_change_check.ended_at) DESC;
+-- Die Änderungen wurden korrekt durchgeführt wurden.
 -- wir wenden die obere Abfrage zum Tauschen der Spalteneinträge auf alle Tabellen an. 
 
 
@@ -129,8 +129,9 @@ ORDER BY TIMESTAMPDIFF(SECOND, column_inverted_check.started_at, column_inverted
 Die benötigten Daten sind nun bereinigt. Wir können mit der Analyse beginnen. 
 
 Wir schauen uns zunächst die minimale, maximale und durchschnittliche Fahrzeit nach Kundentypen an. 
-Da wir alle folgenden Abfragen nur auf für uns relevante Spalten beziehen wollen, erzeugen wir eine temporäre Tabelle mit den für uns wichtigen Spalten, um die Abfragen übersichtlicher zu halten.
-Alle Fahrzeiten werden auf 2 Nachkommastellen gerundet. */
+Alle folgenden Abfragen wollen wir nur auf relevante Spalten beziehen. 
+Zu diesem Zweck erzeugen wir eine temporäre Tabelle mit den für uns wichtigen Spalten, um die Abfragen übersichtlicher zu halten.
+Alle Zeiten werden auf 2 Nachkommastellen gerundet. */
 
 
 -- Erzeugen der Temporären Tabelle mit dem Namen RideDuration  
@@ -178,7 +179,7 @@ WHERE WEEKDAY(started_at) = 5
 	OR WEEKDAY(started_at) = 6
 GROUP BY member_casual; 
 
--- Mietdauer nach Nutzertyp unter der Woche 
+-- Mietdauer nach Nutzertyp an Arbeitstagen
 SELECT
 	member_casual,
 	ROUND(AVG(ride_time_minutes), 2) AS workday_ridetime_minutes
@@ -201,7 +202,7 @@ SELECT
 FROM RideDuration
 GROUP BY member_type;
 
--- Anzahl der Casuals und Members, die am Wochenende mieten 
+-- Anzahl an Fahrten durch der Casuals und Members am Wochenende 
 SELECT
 	member_casual AS member_type,
 	COUNT(member_casual) AS quantity
@@ -211,7 +212,7 @@ WHERE
 	OR WEEKDAY(started_at) = 6
 GROUP BY member_casual;
 
--- Anzahl der Casuals und Members, die unter der Woche mieten 
+-- Anzahl an Fahrten durch der Casuals und Members an Arbeitstagen
 SELECT
 	member_casual AS member_type,
 	COUNT(member_casual) AS quantity
